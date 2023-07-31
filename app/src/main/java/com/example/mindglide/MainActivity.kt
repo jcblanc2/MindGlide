3bf1d7def1de1fb9a2172168a857839d6a7ffedd
jcblanc2 <jclaytonblanc@gmail.com>
2023-07-31 17:33:05 -0400

    Add logic to show the answer side when the card is tapped

--- app/src/main/java/com/example/mindglide/MainActivity.kt
+++ app/src/main/java/com/example/mindglide/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mindglide
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,9 +15,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mindglide.ui.theme.MindGlideTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Look up the question and answer textview and  in layout
+        val tvFlashcardQuestion = findViewById<TextView>(R.id.tvFlashcardQuestion)
+        val tvFlashcardAnswer = findViewById<TextView>(R.id.tvFlashcardAnswer)
+
+        // Set an onClickListener for the question text view
+        tvFlashcardQuestion.setOnClickListener {
+            tvFlashcardAnswer.visibility = View.VISIBLE
+        }
     }
 }
