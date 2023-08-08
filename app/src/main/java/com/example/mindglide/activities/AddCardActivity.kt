50df67e8d268cdd40062f90a71cc25a8303ecc65
jcblanc2 <jclaytonblanc@gmail.com>
2023-08-08 16:23:14 -0400

    Add a Save button that will dismiss 'Add Card Screen' and return to the Main Activity

--- app/src/main/java/com/example/mindglide/activities/AddCardActivity.kt
+++ app/src/main/java/com/example/mindglide/activities/AddCardActivity.kt
@@ -1,20 +1,46 @@
 package com.example.mindglide.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 import com.example.mindglide.R
 
 class AddCardActivity : AppCompatActivity() {
+    private lateinit var etQuestion : EditText
+    private lateinit var etAnswer : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
 
-        // Look up the cancel icon in layout
-        val cancelFloatingBtn = findViewById<ImageView>(R.id.cancelFloatingBtn)
+        // Look up the question, answer editText and the cancel, save button in layout
+        etQuestion = findViewById(R.id.etQuestion)
+        etAnswer = findViewById(R.id.etAnswer)
+        val cancelBtn = findViewById<ImageView>(R.id.ivCancelBtn)
+        val saveBtn = findViewById<ImageView>(R.id.ivSaveBtn)
 
-        cancelFloatingBtn.setOnClickListener{
+        // Add onClickListener to the cancel and save button
+        cancelBtn.setOnClickListener{
             finish()
         }
+
+        saveBtn.setOnClickListener {
+            onSave()
+        }
+    }
+
+    // closes AddCardActivity and returns to MainActivity and put our data
+    private fun onSave() {
+        val data = Intent()
+
+        // Pass relevant data back as a result
+        data.putExtra("question", etQuestion.text.toString())
+        data.putExtra("answer", etAnswer.text.toString())
+
+        setResult(RESULT_OK, data) // set result code and bundle data for response
+
+        finish() // closes the activity, pass data to parent
     }
 }
--- app/src/main/java/com/example/mindglide/activities/MainActivity.kt
+++ app/src/main/java/com/example/mindglide/activities/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mindglide.activities
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mindglide.R
 
@@ -13,13 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Look up the plus icon in layout
-        val addFloatingBtn = findViewById<ImageView>(R.id.addFloatingBtn)
+        // Look up the question, answer textview and the add button in layout
+        val tvFlashcardQuestion = findViewById<TextView>(R.id.tvFlashcardQuestion)
+        val tvFlashcardAnswer = findViewById<TextView>(R.id.tvFlashcardAnswer)
+        val addBtn = findViewById<ImageView>(R.id.ivAddBtn)
 
-        // Add onClickListener to the plus icon
-        addFloatingBtn.setOnClickListener {
+        // This extracts any data that was passed back from AddCardActivity
+        val addCardActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+
+                if (data != null) {
+                    val question = data.getStringExtra("question")
+                    val answer = data.getStringExtra("answer")
+
+                    tvFlashcardQuestion.text = question
+                    tvFlashcardAnswer.text = answer
+                }
+            }
+        }
+
+        // Add onClickListener to the plus icon and question textview
+        addBtn.setOnClickListener {
             val intent = Intent(this, AddCardActivity::class.java)
-            startActivity(intent)
+            addCardActivityResultLauncher.launch(intent)
+        }
+
+        tvFlashcardQuestion.setOnClickListener {
+            tvFlashcardQuestion.visibility = View.INVISIBLE
+            tvFlashcardAnswer.visibility = View.VISIBLE
         }
     }
 }
